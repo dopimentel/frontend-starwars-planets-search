e399430b9b38b7c00606dd648cdc62cf075f9a27
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-04-23 02:47:04 -0300

    Requisito 10

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { queryAllByTestId, queryByTestId, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import AppProvider from '../context/AppProvider';
 import fetch from "../../cypress/mocks/fetch.js";
-
 
 describe('App forms filters testes', () => {
   beforeEach(async () => {
@@ -36,17 +35,61 @@ describe('App forms filters testes', () => {
 
     userEvent.type(nameInput, 'Tatooine');
     expect(planetAlderaan).not.toBeInTheDocument();
+
+    
+    // const otherPlanet = screen.queryByText(/alderaan/i);
+    // expect(otherPlanet).toBeNull();
+    // const planets = await screen.findAllByRole("cell");
+    // expect(planets.length).toBe();
  
   });
 
-  it("test if the delete filter buttons are showed when the filters are used", async () => {
-
+  it("test if the delete filter buttons are showed when the filters are used and it is removed a filter of the filter list when it is alredy been used", async () => {
+    // const column = screen.getByRole("combobox", { name: /coluna/i });
+    // userEvent.selectOptions(column, "diameter");
+    // const numberInput = screen.getByRole("spinbutton", { name: /número/i });
+    // userEvent.type(numberInput, '10000')
     const filterBtn = screen.getByRole("button", { name: /filtrar/i });
     userEvent.click(filterBtn);
     userEvent.click(filterBtn);
     const deleteBtns = screen.getAllByRole("button", { name: /x/i });
-    expect(deleteBtns.length).toBe(2);
+    expect(deleteBtns).toHaveLength(2);
   })
+
+  it("test if the delete filter button remove just a button", async () => {
+    const column = screen.getByRole("combobox", { name: /coluna/i });
+    userEvent.selectOptions(column, "population");
+    const operation = screen.getByRole("combobox", { name: /operação/i });
+    userEvent.selectOptions(operation, "menor que");
+    const numberInput = screen.getByRole("spinbutton", { name: /número/i });
+    userEvent.type(numberInput, "2000000");
+    const filterBtn = screen.getByRole("button", { name: /filtrar/i });
+    userEvent.click(filterBtn);
+    userEvent.selectOptions(operation, "igual a");
+    userEvent.click(filterBtn);
+    userEvent.click(filterBtn);
+    const deleteBtns = screen.getAllByRole("button", { name: /x/i });
+    expect(deleteBtns).toHaveLength(3);
+    userEvent.click(deleteBtns[0]);
+    const newDeleteBtns = screen.getAllByRole("button", { name: /x/i });
+    expect(newDeleteBtns).toHaveLength(2);
+  });
+
+  it("test if the remove all filters button is working", async () => {
+    // const column = screen.getByRole("combobox", { name: /coluna/i });
+    // userEvent.selectOptions(column, "diameter");
+    // const numberInput = screen.getByRole("spinbutton", { name: /número/i });
+    // userEvent.type(numberInput, '10000')
+    const filterBtn = screen.getByRole("button", { name: /filtrar/i });
+    userEvent.click(filterBtn);
+    userEvent.click(filterBtn);
+    const removefiltersBtn = screen.getByRole("button", {
+      name: /remover filtros/i,
+    });
+    userEvent.click(removefiltersBtn);
+    const deleteBtns = screen.queryAllByRole("button", { name: /x/i });
+    expect(deleteBtns).toHaveLength(0);
+  });
 
   it("When it is filtered by diameter bigger then 10000 Bespin e showed. When operation is less then 10000 Bespin is not showed ", async () => {
     const column = screen.getByRole("combobox", { name: /coluna/i });
@@ -56,5 +99,14 @@ describe('App forms filters testes', () => {
     const planeBespin = screen.getByRole("cell", { name: /bespin/i });
     expect(planeBespin).toBeInTheDocument();
     
+    // userEvent.type(nameSeach, "Tatooine");
+    // waitFor(async () => {
+    //   const planet = await screen.findByText(/Tatooine/i);
+    //   expect(planet).toBeInTheDocument();
+    //   const otherPlanet = screen.queryByText(/alderaan/i);
+    //   expect(otherPlanet).toBeNull();
+    // });
   });
+
+
 })
