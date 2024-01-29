d8700b0252829c208c9e3eaa24a2674d62a76b85
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-01-29 03:59:39 -0300

    CSS header finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import { ReactTyped } from 'react-typed';
+import { useEffect } from 'react';
 import FilterForm from './components/FilterForm';
 import Table from './components/Table';
 import AppProvider from './context/AppProvider';
 import SorterForm from './components/SorterForm'; // Import the SorterForm component
 
 function App() {
+  const timeToShrink = 3000;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const header = document.querySelector('.App-header');
+      if (header) {
+        header.classList.add('shrink');
+      }
+    }, timeToShrink); // 3000 milissegundos = 3 segundos
+
+    return () => clearTimeout(timer); // Limpar o timer se o componente for desmontado
+  }, []);
   return (
     <AppProvider>
       <main className="App">
