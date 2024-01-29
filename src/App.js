537f7f950ad55e108c09e84266e8855fafd3884a
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-01-29 03:33:11 -0300

    refact - CSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
+import { ReactTyped } from 'react-typed';
 import FilterForm from './components/FilterForm';
 import Table from './components/Table';
 import AppProvider from './context/AppProvider';
 import SorterForm from './components/SorterForm'; // Import the SorterForm component
 
 function App() {
-  const [showTitle, setShowTitle] = useState(false);
-
-  // Função para mostrar o título gradualmente
-  const handleShowTitle = () => {
-    setShowTitle(true);
-  };
-
-  // UseEffect para ativar a transição ao montar o componente
-  useEffect(() => {
-    handleShowTitle();
-  }, []);
   return (
     <AppProvider>
       <main className="App">
         <header className="App-header">
-          <h1 className={ `App-title ${showTitle ? 'visible' : ''}` }>
-            Star Wars Project
-          </h1>
+          <ReactTyped strings={ ['Star Wars Planets'] } typeSpeed={ 200 } />
         </header>
         <section>
           <FilterForm />
